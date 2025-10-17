8eacd454aa2ee8da5410f48f895963e5ce86b89a
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-10-17 21:39:44 +0530

    just created instaCache

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+
+})
+
+app.post("/content",(req,res)=>{
+
+})
+
+app.get("/content",(req,res)=>{
+
+})
+
+app.post("/brain/share",(req,res)=>{
+
+})
+
+app.get("/brain/:sharelink",(req,res)=>{
+
+})
