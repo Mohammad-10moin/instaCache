d42800e6c53088a17687ddd1cf5d7e9380313108
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-11-14 19:18:18 +0530

    completed all endpoints

--- src/db.ts
+++ src/db.ts
@@ -7,18 +7,42 @@ mongoose.connect(process.env.MongoDB_URL || "");
 
 // mongoose.connect(process.env.MongoDB_URL!); If we are 100% sure that process.env.MongoDB_URL is a string and exists then we can use this syntax.
 
+//defining user schema -- skeleton
+// then defining the model 
+
 const UserSchema = new Schema({
-    username: {type:String, unique:true},
-    password: String
+    username: {type:String, unique:true,required:true},
+    password: {type:String,required:true}
 })
 
 export const UserModel = model("user",UserSchema);
 
+
+//defining the Tag schema
+const TagSchema = new Schema({
+    title:{type: String, required:true}
+})
+
+export const TagModel = model("Tag",TagSchema);
+
+
+//defining the content schema and model
+const contentTypes = ['image','audio','video','article']
 const ContentSchema = new Schema({
-    title:String,
-    link:String,
-    tags:[{type:mongoose.Types.ObjectId,ref:'Tag'}],
+    title:{type: String, required:true},                            //title of the content
+    link:{type: String, required:true},                             //url
+    type:{type: String, enum:contentTypes, required:true},          // document|tweet|video
+    tags:[{type:mongoose.Types.ObjectId,ref:'Tag'}],                //['productivity','news',...]
     userId:{type:mongoose.Types.ObjectId,ref:'user',required:true}
 })
 
 export const ContentModel = model("content",ContentSchema);
+
+//defining link schema 
+const linkSchema = new Schema({
+    token:{type:String,required:true},
+    userId:{type:mongoose.Types.ObjectId, ref:'user',required:true},
+    Permissions:String
+})
+
+export const linkModel = model('link',linkSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
-import { ContentModel, UserModel } from "./db.js";
+import { ContentModel, linkModel, UserModel } from "./db.js";
 import { jwtpswd } from "./config.js";
 import { userMiddleware } from "./middleware.js";
 const app = express();
@@ -85,12 +85,74 @@ app.get("/content",userMiddleware,async(req,res)=>{
     }
 })
 
-app.post("/brain/share",(req,res)=>{
+app.post("/brain/share",userMiddleware,async(req,res)=>{
+    // @ts-ignore
+    const userId = req.userId;
 
+    const crypto = await import('crypto');
+    const token = crypto.randomBytes(16).toString('hex');
+
+    const link = await linkModel.create({
+        token:token,
+        userId:userId,
+        Permissions:'read'
+    })
+    const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+    const shareLinkUrl = `${baseUrl}/brain/${token}`;
+
+    if(link){
+        res.status(200).json({
+            msg: "Brain shared successfully"
+        })
+    }
+    else{
+        res.status(403).json({
+            msg:"user not verified"
+        })
+    }
 })
 
-app.get("/brain/:sharelink",(req,res)=>{
+app.get("/brain/:sharelink",async(req,res)=>{
+    const token = req.params.sharelink;
+    const sharedcontent = await linkModel.findOne({token})
 
+    if(!sharedcontent){
+        res.status(403).json({
+            msg: "Invalid sharelink"
+        })
+    }
+
+    const content = await ContentModel.find({userId:sharedcontent?.userId});
+    if(content){
+        res.json({
+        msg: "Success",
+        content
+      });
+    }
+    else{
+        res.json({
+            msg:"No content found"
+        })
+    }
 })
 
+app.delete("/content",userMiddleware,async(req,res)=>{
+    // @ts-ignore
+    const user = req.userId;
+    const contentid = req.body.contentId;
+    const contentdeleted= await ContentModel.deleteMany({
+        contentid,
+        userId:user
+    })
+    if(contentdeleted){
+        res.status(200).json({
+            msg: "content deleted successfully"
+        })
+    }
+    else{
+        res.status(403).json({
+            msg: "no such content found"
+        })
+    }
+})
 app.listen(3000);
