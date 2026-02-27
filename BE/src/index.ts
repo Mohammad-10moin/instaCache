d8cce94aa41e0e8f40913418081af84729958ba3
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2026-02-27 16:04:28 +0530

    created endpoints and added dependencies in ts

---
+++ BE/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app = express();
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+
+})
+
+app.post("/addContent",(req,res)=>{
+
+})
+
+app.get("/content",(req,res)=>{
+
+})
+
+app.delete("/content",(req,res)=>{
+
+})
+
+app.post("/share",(req,res)=>{
+
+})
+
+app.get("/brain",(req,res)=>{
+    
+})
