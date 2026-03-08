a69d4f10f6e2d2efc898afa842883a8eb10de1fd
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2026-03-08 23:57:52 +0530

    updated

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -6,10 +6,12 @@ import { Card } from './components/Card'
 function App() {
 
   return (
-    <div>
-      <Button variant="primary" text="share Brain" StartIcon={<ShareIcon/>}></Button>
-      <Button variant="secondary" text="add Content" StartIcon={<Plusicon/>}></Button>
-      <div className='flex'>
+    <div className='p-4'>
+      <div className='flex justify-end gap-4'>
+        <Button variant="primary" text="share Brain" StartIcon={<ShareIcon/>}></Button>
+        <Button variant="secondary" text="add Content" StartIcon={<Plusicon/>}></Button>
+      </div>
+      <div className='flex gap-4'>
         <Card title="Nasheed" link='https://www.youtube.com/watch?v=21WaGYBlb9M' type='youtube'/>
         <Card title="tweet" link='https://x.com/im10Moin/status/1960411425081581991?s=20' type='twitter'/>
       </div>
