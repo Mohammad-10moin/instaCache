681d37bd3f54f47307d14d95c0ccf16c06aded92
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2026-03-15 00:40:33 +0530

    made improvements

--- FE/src/components/SideItem.tsx
+++ FE/src/components/SideItem.tsx
@@ -4,7 +4,10 @@ export function SidebarItem({text,icon}:{
     text:string;
     icon:ReactElement;
 }){
-    return <div className="flex items-center justify-center border rounded m-2 p-2">
-        {icon} {text}
+    return <div className="flex items-center justify-center border rounded m-2 p-2 max-w-64 ">
+        <div className="p-2">
+            {icon} 
+        </div>
+        {text}
     </div>
 }
--- FE/src/components/Sidebar.tsx
+++ FE/src/components/Sidebar.tsx
@@ -3,7 +3,10 @@ import { YoutubeIcon } from "../icons/youtubeicon";
 import { SidebarItem } from "./SideItem";
 
 export function Sidebar(){
-    return <div className="h-screen w-72 bg-white border-gray-300 border-r-2 left-0 top-0 fixed">
+    return <div className="h-screen w-72 bg-white border-gray-300 border-r-2 left-0 top-0 fixed pl-2">
+        <div className="flex text-3xl pl-2 py-2">
+            Brainly
+        </div>
         <div className="pt-4">
             <SidebarItem text="Youtube" icon={<YoutubeIcon/>}/>
             <SidebarItem text="Twitter" icon={<TwitterIcon/>}/>
