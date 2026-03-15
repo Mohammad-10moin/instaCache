b52ef3863efb8c88130a69b2f530dbdfd60b1543
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2026-03-16 01:31:23 +0530

    added routes using react-router-dom

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,13 +2,15 @@ import './App.css'
 import Dashboard from './pages/Dashboard'
 import { Signin } from './pages/Signin'
 import { Signup } from './pages/Signup'
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 function App() {
-  return (
-      <>
-      <Signin/>
-      </>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/home' element={<Dashboard/>}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
